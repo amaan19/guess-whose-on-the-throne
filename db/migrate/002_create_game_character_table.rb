d3d854eb5aa7b29e1975156702465c3c9f2db9e4
amaan19 <amaan.ahmed3@gmail.com>
2019-01-03 19:18:02 +0000

    Ironed out major bugs in initialization and main menu

--- db/migrate/002_create_game_character_table.rb
+++ db/migrate/002_create_game_character_table.rb
@@ -1,8 +1,8 @@
 class CreateGameCharacterTable < ActiveRecord::Migration[4.2]
   def change
-    create_table :game_character do |t|
+    create_table :game_characters do |t|
       t.integer :game_id
       t.integer :character_id
-    end 
+    end
   end
 end
